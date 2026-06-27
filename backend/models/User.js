52f4f6457a82ef057e6d32ffd103ebcae0fe0c5d
Devesh Pandey <pandeydevesh33@gmail.com>
2026-06-27 21:17:48 +0530

    fix: refactor pre-save password hook to standard async schema format to resolve next is not a function

--- backend/models/User.js
+++ backend/models/User.js
@@ -32,14 +32,13 @@ const UserSchema = new mongoose.Schema({
 }, { timestamps: true });
 
 // Hash password before saving if modified
-UserSchema.pre('save', async function(next) {
-    if (!this.isModified('password')) return next();
+UserSchema.pre('save', async function() {
+    if (!this.isModified('password')) return;
     try {
         const salt = await bcrypt.genSalt(10);
         this.password = await bcrypt.hash(this.password, salt);
-        next();
     } catch (err) {
-        next(err);
+        throw err;
     }
 });
 
